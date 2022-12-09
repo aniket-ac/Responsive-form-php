52c5e8ba912ca99906c00ff076ed4da74bd55c0a
aniket-ac <68229404+aniket-ac@users.noreply.github.com>
2022-12-09 19:35:00 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<div class="w-50 m-auto">
+                <form action="about.php" method="POST">
+                    <div class="form-group">
+                        <label>Name:</label>
+                        <input type="text" name="name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Email:</label>
+                        <input type="email" name="email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>NUmber:</label>
+                        <input type="text" name="number" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-success">Submit </button>
+                </form>
+            </div>
